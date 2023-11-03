a9a9ecae43e45c2d0d479c049d3a23faa38bfe9d
dallas <dallasmgere@gmail.com>
2023-11-02 20:27:04 -0500

    I got it working

---
+++ SwiftCounter/SwiftCounter/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  SwiftCounter
+//
+//  Created by Dallas Gere on 11/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var count: Int = 0
+    var body: some View {
+        VStack {
+            Text("\(count)")
+            Spacer().frame(height: 20)
+            HStack {
+                Button {
+                    decrease()
+                } label: {
+                    Text("decrease")
+                        .padding()
+                        .foregroundColor(.black)
+                        .background(
+                            RoundedRectangle(
+                                cornerRadius: 25,
+                                style: .continuous
+                            )
+                        )
+                        .overlay {
+                            
+                            RoundedRectangle(
+                                cornerRadius: 25,
+                                style: .continuous
+                            )
+                            .stroke(.teal, lineWidth: 2)
+                        }
+                }
+                
+                Button {
+                    increase()
+                } label: {
+                    Text("decrease")
+                        .padding()
+                        .foregroundColor(.black)
+                        .background(
+                            RoundedRectangle(
+                                cornerRadius: 25,
+                                style: .continuous
+                            )
+                        )
+                        .overlay {
+                            
+                            RoundedRectangle(
+                                cornerRadius: 25,
+                                style: .continuous
+                            )
+                            .stroke(.teal, lineWidth: 2)
+                        }
+                }
+            }
+        }
+    }
+    
+    func increase() {
+        count += 1
+    }
+    
+    func decrease() {
+        count -= 1
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftCounter/SwiftCounter/SwiftCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCounterApp.swift
+//  SwiftCounter
+//
+//  Created by Dallas Gere on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
